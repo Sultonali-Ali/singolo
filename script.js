a1b19712b4472fbb68c2edbd12fe55d15159faf6
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-03-16 12:01:25 +0500

    doc: update

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const changeHeaderMenuHandler = () => {
     document.querySelector('.header__navigation .navigation').addEventListener('click', (e) => {
         if (e.target.classList.contains('navigation__item')) {
             let clickedMenuTab = e.target;
+            scrollToClickedSection();
             removeSelectedTab();
             selectClickedMenuTab(clickedMenuTab);
         }
@@ -27,3 +28,21 @@ const removeSelectedTab = () => {
 const selectClickedMenuTab = (clickedMenuTab) => {
     clickedMenuTab.classList.add('navigation__item--active');
 };
+
+const scrollToClickedSection = () => {
+    const anchors = document.querySelectorAll('a.navigation__item');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href')
+
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
+};
+
